4308fbba606acfaa4726db97f25880039a8ad218
shao <ligeard.pro@gmail.com>
2022-08-19 17:25:32 +0200

    oops paths

--- front/src/components/InputField.jsx
+++ front/src/components/InputField.jsx
@@ -4,8 +4,8 @@ import React from "react";
 function InputField({name,type,placeholder,onChange,idName}){
     return (
         <div class="inputField">
-            <label className="inputField-label" for={name}>{name}</label>
-            <input className="inputField-input" onChange={onChange} type={type} name={name} placeholder={placeholder} id={idName}/>
+            <label className="inputField-label label" for={name}>{name}</label>
+            <input className="inputField-input input" onChange={onChange} type={type} name={name} placeholder={placeholder} id={idName}/>
         </div>
     )
 }
