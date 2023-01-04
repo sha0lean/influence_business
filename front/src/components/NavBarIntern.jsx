74d4ded901136200c582ea1266caac2e8979b713
sha0lean <ligeard.pro@gmail.com>
2023-01-05 00:42:48 +0100

    add responsive home, add nav intern and correct responsive on register and login

---
+++ front/src/components/NavBarIntern.jsx
@@ -0,0 +1,72 @@
+import React, {useState} from "react";
+import {
+    Link
+} from "react-router-dom";
+import {api} from "../configApi.js";
+import "../assets/scss/navBarIntern.scss";
+
+import {getToken,removeToken} from "../utils/localStorage/useToken.js";
+import {getRole,removeRole} from "../utils/localStorage/useRole.js";
+
+
+async function logoutUser(credentials){
+    return fetch(api.url + "/logout", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json());
+}
+
+
+
+function NavBar(){
+    const logout = async e => {
+        e.preventDefault();
+        
+        const response = await logoutUser({
+            token: getToken()
+        })
+        if(response.message === "The user has been disconnected"){
+            removeToken()
+            removeRole()
+            window.location.reload().then(() => {
+                setToken(getToken(null))
+                setRole(getRole(null))
+            });
+            
+        }
+        else{
+            alert("Mauvais identifiants")
+        }
+    }
+    const [token,setToken] = useState(getToken())
+    const [role,setRole] = useState(getRole())
+
+    return (
+        <div id="mainContainerNavIntern">
+            <div id="containerNavIntern">
+                    <nav>
+                        <div id="containerPartLeftNav">
+                            <div id="containerLogo">
+                                <Link to="/" className="lato"><img src={require("../assets/images/logo_ib.png")}  alt="logo influenceur business"/></Link>
+                            </div>
+                            <div id="containerLinkPartLeft">
+                                <Link to="/dashboard" className="lato">Tableau de bord</Link>
+                                <Link to="/role" className="lato">{getRole()}</Link>
+                            </div>
+                        </div>
+                        <div id="containerPartRightNav">
+                            <img src={require("../assets/images/loupe.png")}/>
+                            <img src={require("../assets/images/question.png")}/>
+                            <img src={require("../assets/images/profil.png")}/>
+                        </div>
+                    </nav>
+                    
+            </div>
+        </div>
+    )
+}
+export default NavBar;
--- front/src/components/layout/Layout.js
+++ front/src/components/layout/Layout.js
@@ -1,9 +1,13 @@
 import React from "react";
+import NavBarIntern from "../NavBarIntern.jsx";
 import NavBar from "../NavBar.jsx";
-const Layout = ({children}) => {
+import {getToken} from "../../utils/localStorage/useToken.js";
+import {getRole} from "../../utils/localStorage/useRole.js";
+const Layout = ({children}) => {    
     return (
         <div>
-            <NavBar/>
+            {!getToken() && !getRole() && <NavBar/>}
+            {getToken() && getRole() && <NavBarIntern />}
             <main>{children}</main>
         </div>
     )
