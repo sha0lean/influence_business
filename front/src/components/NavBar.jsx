f7b4ecf2bc10bc4e7dbfd360b8b856cbb5e7bf52
sha0lean <ligeard.pro@gmail.com>
2023-01-06 20:46:23 +0100

    primary nav

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -1,15 +1,22 @@
-import React, {useState} from "react";
+import React, {
+    useState,
+    useEffect
+} from "react";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 import {
-    Link
+    Link,
+    useNavigate
 } from "react-router-dom";
-import {api} from "../configApi.js";
-import "../assets/scss/navBar.scss";
+import { api } from "../configApi.js";
+// import "../assets/scss/navBar.scss";
+import "../assets/scss/navBar2.scss";
 
-import {getToken,removeToken} from "../utils/localStorage/useToken.js";
-import {getRole,removeRole} from "../utils/localStorage/useRole.js";
+import { getToken, removeToken } from "../utils/localStorage/useToken.js";
+import { getRole, removeRole } from "../utils/localStorage/useRole.js";
 
 
-async function logoutUser(credentials){
+async function logoutUser(credentials) {
     return fetch(api.url + "/logout", {
         method: 'POST',
         headers: {
@@ -20,54 +27,107 @@ async function logoutUser(credentials){
         .then(data => data.json());
 }
 
+function NavBar() {
+    // ——————————————————————————————————————————
+    const navigate = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [size, setSize] = useState({
+        width: 0,
+        height: 0,
+    });
+    useEffect(() => {
+        const handleResize = () => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener("resize", handleResize);
 
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
-function NavBar(){
+    useEffect(() => {
+        if (size.width > 768 && menuOpen) {
+            setMenuOpen(false);
+        }
+    }, [size.width, menuOpen]);
+
+    const menuToggleHandler = () => {
+        setMenuOpen((p) => !p);
+    };
+    // ——————————————————————————————————————————
+
     const logout = async e => {
         e.preventDefault();
-        
+
         const response = await logoutUser({
             token: getToken()
         })
-        if(response.message === "The user has been disconnected"){
+        if (response.message === "The user has been disconnected") {
             removeToken()
             removeRole()
             window.location.reload().then(() => {
                 setToken(getToken(null))
                 setRole(getRole(null))
             });
-            
+
         }
-        else{
+        else {
             alert("Mauvais identifiants")
         }
     }
-    const [token,setToken] = useState(getToken())
-    const [role,setRole] = useState(getRole())
+    const [token, setToken] = useState(getToken())
+    const [role, setRole] = useState(getRole())
 
     return (
-        <div id="mainContainerNav">
-            <div id="containerNav">
-                    <nav>
-                        <div id="containerPartLeftNav">
-                            <div id="containerLogo">
-                                <Link to="/"><img src={require("../assets/images/logo_ib.png")}  alt="logo influenceur business"/></Link>
-                            </div>
-                            <div id="containerLinkPartLeft">
-                                <Link to="/">Accueil</Link>
-                                <Link to="/contact">Contact</Link>
-                                <Link to="/inscription">Inscription</Link>
-                            </div>
-                        </div>
-                        <div id="containerButtonRegister">
-                            { !token && <Link to="/connexion">Connexion</Link>}
-                            { token && <Link to="/connexion" onClick={(logout)}>Deconnexion</Link> }
+        <header className="header lato">
+            <div className="header__content">
+                <Link to="/" className="header__content__logo">
+                    Influenceur business
+                </Link>
+                <nav
+                    className={`${"header__content__nav"} 
+          ${menuOpen && size.width < 768 ? `${"isMenu"}` : ""} 
+          }`}
+                >
+                    <ul>
+                        <li>
+                            <Link to="/">Accueil</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
 
-                        </div>
-                    </nav>
-                    
+
+                        <Link to="/inscription">
+                            <button className="btn lato">Inscription</button>
+                        </Link>
+                        <Link to="/login">
+
+
+                            {!token && <Link to="/connexion">
+                                <button className="btn btn__login">
+                                    Connexion
+                                </button>
+                            </Link>}
+                            {token && <Link to="/connexion" onClick={(logout)}>
+                                <button className="btn btn__login">
+                                    Deconnexion
+                                </button>
+                            </Link>}
+                        </Link>
+                    </ul>
+                </nav>
+                <div className="header__content__toggle">
+                    {!menuOpen ? (
+                        <BiMenuAltRight onClick={menuToggleHandler} />
+                    ) : (
+                        <AiOutlineClose onClick={menuToggleHandler} />
+                    )}
+                </div>
             </div>
-        </div>
+        </header>
     )
 }
 export default NavBar;
--- front/src/views/Home.js
+++ front/src/views/Home.js
@@ -3,14 +3,14 @@ import "../assets/scss/home.scss"
 import {
     Link
 } from "react-router-dom";
-function Home(){
-    return(
+function Home() {
+    return (
         <div id="mainContainerHome">
             <div id="containerBgImage">
-                <img src={require("../assets/images/bg_home.png")} alt="background image home"/>
+                <img src={require("../assets/images/bg_home.png")} alt="background image home" />
             </div>
             <div id="containerTitleLogo">
-                <img src={require("../assets/images/logo_ib.png")} alt="logo influenceur business"/>
+                <img src={require("../assets/images/logo_ib.png")} alt="logo influenceur business" />
                 <div id="containerTitle">
                     <h1 className="lato">Influenceur business</h1>
                     <h3 className="lato">There is no way you can't trust us.</h3>
@@ -19,13 +19,13 @@ function Home(){
             </div>
             <div id="containerSubject">
                 <div>
-                    <img src={require("../assets/images/choice_subject.png")} alt="choix du sujet"/>
-                </div>                
-                <div>
-                    <img src={require("../assets/images/modules.png")} alt="choix des modules"/>
+                    <img src={require("../assets/images/choice_subject.png")} alt="choix du sujet" />
                 </div>
                 <div>
-                    <img src={require("../assets/images/organizational_intelligence.png")} alt="organisation"/>
+                    <img src={require("../assets/images/modules.png")} alt="choix des modules" />
+                </div>
+                <div>
+                    <img src={require("../assets/images/organizational_intelligence.png")} alt="organisation" />
                 </div>
             </div>
         </div>
