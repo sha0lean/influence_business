2023b7ce391f787a172eac5484cf30edc7b7d5eb
sha0lean <ligeard.pro@gmail.com>
2023-01-06 19:36:38 +0100

    new primary nav bar

--- front/src/components/layout/Layout.js
+++ front/src/components/layout/Layout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import NavBarIntern from "../NavBarIntern.jsx";
 import NavBar from "../NavBar.jsx";
-import {getToken} from "../../utils/localStorage/useToken.js";
-import {getRole} from "../../utils/localStorage/useRole.js";
-const Layout = ({children}) => {    
+import { getToken } from "../../utils/localStorage/useToken.js";
+import { getRole } from "../../utils/localStorage/useRole.js";
+const Layout = ({ children }) => {
     return (
         <div>
-            {!getToken() && !getRole() && <NavBar/>}
+            {!getToken() && !getRole() && <NavBar />}
             {getToken() && getRole() && <NavBarIntern />}
             <main>{children}</main>
         </div>
