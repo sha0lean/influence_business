1494cf660f5255d29fe2a15028b15360db1d447a
sha0lean <ligeard.pro@gmail.com>
2023-01-06 21:19:24 +0100

    footer

---
+++ front/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "../assets/scss/footer.scss";
+
+function Footer() {
+	return (
+		<footer>
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+			<div class="top_header">
+				<section>
+					<span><i class="fa fa-map-marker"></i></span>
+					<span>4 Avenue industrielle,  Acacias, Genève 1227, CH</span>
+				</section>
+				<section>
+					<span><i class="fa fa-phone"></i></span>
+					<span>+41 78 735 88 41</span>
+				</section>
+				<section>
+					<span><i class="fa fa-envelope"></i></span>
+					<span>info@influencerbusiness.ch</span>
+				</section>
+			</div>
+			<span class="border-shape"></span>
+			<div class="bottom_content">
+				<section>
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-instagram"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-telegram"></i></a>
+				</section>
+				<section>
+					<a href="/">Home</a>
+					<a href="#">About us</a>
+					<a href="#">Member</a>
+					<a href="/contact">Contact Us</a>
+				</section>
+			</div>
+			<div class="copyright">
+				Copyright © 2021 Influenceur Business — All rights reserved
+			</div>
+		</footer >
+	)
+}
+export default Footer;
--- front/src/components/layout/Layout.js
+++ front/src/components/layout/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBarIntern from "../NavBarIntern.jsx";
 import NavBar from "../NavBar.jsx";
+import Footer from "../Footer.jsx";
 import { getToken } from "../../utils/localStorage/useToken.js";
 import { getRole } from "../../utils/localStorage/useRole.js";
 const Layout = ({ children }) => {
@@ -9,6 +10,8 @@ const Layout = ({ children }) => {
             {!getToken() && !getRole() && <NavBar />}
             {getToken() && getRole() && <NavBarIntern />}
             <main>{children}</main>
+            allo
+            <Footer />
         </div>
     )
 }
